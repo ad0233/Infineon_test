02389ee32b629cc1aaf9550050143856ebf4de6a
HenryLiang <lzh@axiarz.com>
2025-12-30 10:32:48 +0800

    导入雷达，并调用，发现闭包了

---
+++ components/my_lidar_inf/my_lidar_inf.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "esp_err.h"
+#include "stdint.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int my_lidar_inf_init(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ components/my_lidar_inf/resource_map.h
@@ -0,0 +1,16 @@
+#include "driver/gpio.h"
+
+/* sensor SPI interface */
+#define PIN_XENSIV_BGT60TRXX_SPI_SCLK       GPIO_NUM_23 //
+#define PIN_XENSIV_BGT60TRXX_SPI_MOSI       GPIO_NUM_21 //
+#define PIN_XENSIV_BGT60TRXX_SPI_MISO       GPIO_NUM_20 //
+#define PIN_XENSIV_BGT60TRXX_SPI_CSN        GPIO_NUM_12 //
+/* sensor interrupt output pin */
+#define PIN_XENSIV_BGT60TRXX_IRQ            GPIO_NUM_22 //
+/* sensor HW reset pin */
+#define PIN_XENSIV_BGT60TRXX_RSTN           GPIO_NUM_13
+/* enable 1V8 LDO on radar wingboard*/
+#define PIN_XENSIV_BGT60TRXX_LDO_EN         GPIO_NUM_14
+#define PIN_LED_RED                         GPIO_NUM_15
+#define PIN_LED_GREEN                       GPIO_NUM_16
+#define PIN_LED_BLUE                        GPIO_NUM_17
